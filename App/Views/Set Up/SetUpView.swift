97604d2a6c37da7b59d87b121cde42f44fba917b
katagaki <68231035+katagaki@users.noreply.github.com>
2024-03-10 13:42:19 +0900

    Localize Set Up view

--- App/Views/Set Up/SetUpView.swift
+++ App/Views/Set Up/SetUpView.swift
@@ -13,38 +13,29 @@ struct SetUpView: View {
         NavigationStack {
             List {
                 Section {
-#if os(iOS) && !targetEnvironment(macCatalyst)
-                    Text(verbatim: "1. Open the Settings app.")
-                    Text(verbatim: "2. Scroll down and tap 'Safari.'")
-                    Text(verbatim: "3. Tap 'Extensions'.")
-                    Text(verbatim: "4. Tap 'Unagent'.")
-                    Text(verbatim: "5. Toggle Unagent on for the profiles you wish to use Unagent in.")
-#else
-                    Text(verbatim: "1. Open the Safari app.")
-                    Text(verbatim: "2. From the menu bar, select 'Safari' > 'Preferences...'.")
-                    Text(verbatim: "3. Select the Extensions tab.")
-                    Text(verbatim: "4. Select 'Unagent'.")
-                    Text(verbatim: "5. Turn Unagent on by checking it.")
-#endif
+                    Text("SetUp.Step1")
+                    .lineSpacing(4.0)
+                    #if targetEnvironment(macCatalyst)
+                    Link(destination: URL(string: "https://www.example.com")!, label: {
+                        Text("SetUp.OpenSafari")
+                    })
+                    #else
+                    Link(destination: URL(string: UIApplication.openSettingsURLString)!, label: {
+                        Text("SetUp.OpenSettings")
+                    })
+                    #endif
                 } header: {
-                    ListSectionHeader(text: "1. Turn On Unagent")
+                    ListSectionHeader(text: "SetUp.Step1.Title")
                         .font(.body)
                 }
                 Section {
-#if os(iOS) && !targetEnvironment(macCatalyst)
-                    Text(verbatim: "1. Open the Safari app.")
-                    Text(verbatim: "2. Tap the 'Aa' icon to the left of the address bar.")
-                    Text(verbatim: "3. Tap 'Unagent'.")
-                    Text(verbatim: "4. Tap 'Always Allow...'.")
-                    Text(verbatim: "5. Tap 'Always Allow on Every Website'.")
-#else
-                    Text(verbatim: "1. Open the Safari app.")
-                    Text(verbatim: "2. Select the Unagent icon on the toolbar.")
-                    Text(verbatim: "3. Select 'Always Allow on Every Website...'.")
-                    Text(verbatim: "4. Select 'Always Allow on Every Website'.")
-#endif
+                    Text("SetUp.Step2")
+                    .lineSpacing(4.0)
+                    Link(destination: URL(string: "https://www.example.com")!, label: {
+                        Text("SetUp.OpenSafari")
+                    })
                 } header: {
-                    ListSectionHeader(text: "2. Give Unagent Access")
+                    ListSectionHeader(text: "SetUp.Step2.Title")
                         .font(.body)
                 }
             }
